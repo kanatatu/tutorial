7d038a2f9f7cebb0676e80abe88aa41e42795538
kanatatu <43596260+kanatatu@users.noreply.github.com>
2018-09-26 14:48:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[256];
-    fgets(*str, sizeof(str), stdin);
+    char *str[256];
+    *fgets(*str, sizeof(str), stdin);
     printf("%s\n", "str");
 
     return 0;
